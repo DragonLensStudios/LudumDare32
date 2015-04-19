124480e9af240f5c51c9a7c34d807a09b43608c6
metallicsoul92 <metallicsoul92@aim.com>
2015-04-19 19:35:27 -0400

    l

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 
 public class NPCController : MonoBehaviour {
-
+	
 	public GameObject NPC;
 	public GameObject MainCharacter;
-
 	
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		NPC = this.gameObject;
 	}
 	
-
+	
 }
