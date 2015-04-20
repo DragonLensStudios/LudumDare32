e75e8b77cd6b815dcac34b023c85ec2f2d4a7a1d
Jeffery Miller <Mony@dragonlensstudios.com>
2015-04-20 02:19:47 -0600

    Update Messagebox Nametag
    Added Nametag for Dialog

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -4,18 +4,19 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class NPCController : MonoBehaviour {
-
+	
+	public string npc_name;
 	public GameObject NPC;
 	public GameObject MainCharacter;
 	public List<string> message_dialog;
 	public InteractionNPC talking_player;
-
 	
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		NPC = gameObject;
 	}
 	
-
+	
 }
